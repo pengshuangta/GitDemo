c3e0281a8f8b8e4a2dba99d6799c2b61c6221cfd
pengshuangta <1910070001@qq.com>
2017-09-12 15:57:07 +0800

    1、创建SecondVC

---
+++ GitDemo/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  GitDemo
+//
+//  Created by 彭双塔 on 2017/9/12.
+//  Copyright © 2017年 彭双塔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
